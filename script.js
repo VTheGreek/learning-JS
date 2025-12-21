bf1a23b1e1a044c97f1304e6c2dc4a06bb9f4671
VTheGreek <vasilis.proefth@gmail.com>
2025-12-21 19:42:03 +0200

    Finished 23 exercises tonight

---
+++ script.js
@@ -0,0 +1,187 @@
+// ASK THE USER A NUMBER WITH PROMPT AND MAKE IT APPEAR WITH ALERT
+
+// const num = prompt("Choose any number: ");
+// alert(num);
+
+// ASK USER FOR THEIR NAME AND MAKE IT APPEAR WITH ALERT
+// let name = prompt("What's your name?");
+// alert(`Hello ${name}`);
+
+// ASK USER FOR THEIR AGE AND CONSOLE.LOG IT
+// let age = prompt("How old are you?");
+// console.log(age);
+
+// CHECK IF A NUMBER IS POSITIVE
+// let num = Number(prompt("Give me any positive number you want: "));
+// 
+// if (num > 0) {
+//     alert(`${num} is a positive number`);
+// } else {
+//     alert(`${num} is not positive number.`);
+// }
+
+// CHECK IF A NUMBER IS NEGATIVE
+// let num = Number(prompt("Give me any negative number you want: "));
+// 
+// if (num < 0) {
+//     alert(`${num} is a negative number.`)
+// } else {
+//     alert(`${num} is not a negative number.`)
+// }
+
+// ASK A NUMBER FROM THE USER AND CHECK IF IT'S ZERO
+// let num = Number(prompt("Give me any number: "));
+// 
+// if (num === 0) {
+//     alert(`${num} is equal to 0`)
+// } else {
+//     alert(`${num} is not 0`)
+// }
+
+// CHECK IF A NUMBER IS EVEN
+
+// let num = Number(prompt("Give me an even number: "));
+// 
+// if (num % 2 === 0) {
+//     alert(`${num} is even number`);
+// } else {
+//     alert(`${num} is not even number`);
+// }
+
+// CHECK IF A NUMBER IS ODD
+
+// let num = Number(prompt("Give me an odd number: "))
+
+// if (num % 2 === 1) {
+//     alert(`${num} is an odd number`)
+// } else {
+//     alert(`${num} is not an odd number`)
+// }
+
+// ASK THE USER FOR 2 NUMBERS AND SHOW THE LARGER ONE
+
+// let num1 = Number(prompt("Give me your first number: "));
+// let num2 = Number(prompt("Give me your second number: "));
+
+// if (num1 > num2) {
+//     alert(num1);
+// } else if (num1 < num2) {
+//     alert(num2);
+// } else {
+//     alert(`${num1} and ${num2} are equal.`)
+// }
+// CHECK THE AGE OF THE USER. IF THE USER IS OVER 18. USER IS ADULT
+
+// let age = Number(prompt("How old are you?"));
+
+// if (age >= 18) {
+//     alert("You are an adult");
+// } else {
+//     alert("You are a minor");
+// }
+
+// ASK FOR A NUMBER AND CHECK IF IT'S LARGER THAN 100
+
+// let num = Number(prompt("Give me a larger number than 100"));
+
+// if (num > 100) {
+//     alert(`${num} is larger than 100`)
+// } else {
+//     alert(`${num} is smaller than 100`)
+// }
+
+// ASK USER FOR THEIR SCORE AND IF IT'S >= 50 THE USER PASSED
+
+// let score = Number(prompt("Write your score"));
+
+// if (score >= 50) {
+//     alert("You passed the exams.")
+// } else {
+//     alert("You have to take the exams again.")
+// }
+
+// ASK THE USER FOR THE TEMPERATURE AND ALERT IF IT'S COLD (TEMPRATURE < 10)
+
+// let degrees = Number(prompt("What is the temperature today?"));
+
+// if (degrees < 10) {
+//     alert("It's cold wear a jacket")
+// } else {
+//     alert("it's warm")
+// }
+
+// Ask the user for a number and display whether it is a multiple of 5.
+
+// let num = Number(prompt("Give me any number"))
+
+// if (num % 5 === 0) {
+//     alert(`${num} is multiple of 5.`)
+// } else {
+//     alert(`${num} is not multiple of 5.`)
+// }
+
+// Ask the user for a number and display whether it is less than 0.
+
+// let num = Number(prompt("Give me a number: "));
+
+// if (num < 0) {
+//     alert(`${num} is smaller than 0`)
+// } else {
+//     alert(`${num} is larger than 0`)
+// }
+
+// Ask the user for a name and display an error message if it is empty.
+
+// let name = prompt("What's your name?")
+
+// if (name === "") {
+//     alert("Error")
+// } else {
+//     alert(`Hello ${name}`);
+// }
+
+// Ask the user for a number and display its square.
+
+// let num = Number(prompt("Give me a number and find out the square of it."))
+// alert(num ** 2)
+
+// Ask the user for two numbers and display their sum.
+
+// let num1 = Number(prompt("Give me your first number"))
+// let num2 = Number(prompt("Give me your second number"))
+// alert(num1 + num2)
+
+// Ask the user for a number and display whether it is equal to 10.
+
+// let num = Number(prompt("Give me a number: "))
+// if (num === 10) {
+//     alert(`${num} is equal to 10.`)
+// } else {
+//     alert(`${num} is not equal to 10.`)
+// }
+
+// let gender = prompt("What is your gender Male(m) or Female(f)");
+
+// if (gender === "m" || gender === "male" || gender === "Male") {
+//     alert("Hello sir")
+// } else if (gender === "f" || gender === "female" || gender === "Female") {
+//     alert("Hello miss")
+// }
+
+// Display numbers from 1 to 10.
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i)
+// } 
+
+// Display numbers from 10 down to 1.
+
+// for (let i = 10; i >= 1; i--) {
+//     console.log(i)
+// }
+
+// Display all even numbers from 1 to 20.
+
+// for (let i = 2; i <= 20; i = i + 2) {
+//     console.log(i)
+// }
